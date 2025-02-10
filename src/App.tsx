eee5102171578f9c4e8fe35ef338ffb49c76e2ea
matt zhou <matt.whats.up.with.the.name@gmail.com>
2025-02-10 16:44:24 +0800

    add function

--- src/App.tsx
+++ src/App.tsx
@@ -3,35 +3,14 @@ import { Counter } from './features/counter/Counter'
 import { Quotes } from './features/quotes/Quotes'
 import logo from './logo.svg'
 
-import React, { FC } from 'react'
-import { transform } from '@babel/standalone'
-
-const jsxCode = `<button onClick={() => alert("Hello!")}>Click Me</button>`
-const jsxCode2 = `<b >Click Me</b>`
-
-type ParsedComponent = FC | null
-
-function parseJSX(jsxString: string): ParsedComponent {
-  try {
-    const transpiledCode = transform(jsxString, { presets: ['react'] }).code
-    if (!transpiledCode) return null
-
-    // 生成一个 React 组件
-    // eslint-disable-next-line no-new-func
-    return new Function('React', `return () => ${transpiledCode}`)(React)
-  } catch (error) {
-    console.error('JSX 解析错误:', error)
-    return null
-  }
-}
 const App = () => {
-  const Comp = parseJSX(jsxCode)
   return (
     <div className="App">
       <header className="App-header">
-        <div dangerouslySetInnerHTML={{ __html: jsxCode2 }}></div>
         {/*Comp && <Comp />*/}
+        <img src={logo} alt="logo" />
         <Counter />
+        <Quotes />
       </header>
     </div>
   )
