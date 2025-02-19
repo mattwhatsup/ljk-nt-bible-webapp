ab473dd358f4aa7a64905454aef713180e5d8ab9
matt zhou <matt.whats.up.with.the.name@gmail.com>
2025-02-19 15:02:24 +0800

    Switch

--- src/components/BookNav.tsx
+++ src/components/BookNav.tsx
@@ -1,8 +1,13 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Text } from '@chakra-ui/react'
+import { Switch } from './ui/switch'
 import BibleSelector from '@/components/BibleSelector/BibleSelector'
+import { useState } from 'react'
+import type { SelectValue } from './BibleSelector/BibleDropDown'
+
 type Props = {}
 
 export default function BookNav({}: Props) {
+  const [selected, setSelected] = useState<SelectValue>()
   return (
     <Box
       as="header"
@@ -15,17 +20,20 @@ export default function BookNav({}: Props) {
       justifyContent={'center'}
       zIndex={99}
     >
-      <Box width={'2xl'}>
+      <Box width={'2xl'} display={'flex'} justifyContent={'space-between'}>
         <BibleSelector
-          selected={{
-            book: 'eph',
-            chapter: 1,
-            verse: 2,
-          }}
+          selected={selected}
           onChange={selected => {
             console.log(selected)
+            setSelected(selected)
           }}
         />
+
+        <Box>
+          <Switch colorPalette={'blue'} defaultChecked mr={1} color={'white'}>
+            显示注释
+          </Switch>
+        </Box>
       </Box>
     </Box>
   )
