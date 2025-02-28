80df564c788ccd0477c07adc873b45ce0dd98465
matt zhou <matt.whats.up.with.the.name@gmail.com>
2025-02-28 17:18:08 +0800

    tsconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // console.log(process.env.NODE_ENV)
