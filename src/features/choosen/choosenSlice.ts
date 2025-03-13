86291ffc0e9d6a7fe3c4f29cd76199b16624ff11
matt zhou <matt.whats.up.with.the.name@gmail.com>
2025-03-13 16:53:58 +0800

    准备处理shiftkey

--- src/features/choosen/choosenSlice.ts
+++ src/features/choosen/choosenSlice.ts
@@ -1,6 +1,7 @@
 import type { PayloadAction } from '@reduxjs/toolkit'
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 import type { RootState } from '@/app/store'
+import type { BookName } from '../book/bookApi'
 
 interface ChoosenState {
   selectedVerses: Record<string, string[]>
@@ -12,38 +13,27 @@ const initialState: ChoosenState = {
   lastSelectedVerse: {},
 }
 
+export const selectVerseThunkAction = createAsyncThunk(
+  'choosen/fetchState',
+  (
+    _: {
+      lang: 'cn' | 'tw'
+      book: string
+      chapter: number
+      verse: string
+      shiftKey?: boolean
+    },
+    { getState },
+  ) => {
+    const state = getState() as RootState
+    return state
+  },
+)
+
 export const choosenSlice = createSlice({
   name: 'choosen',
   initialState,
   reducers: {
-    selectVerse(
-      state,
-      action: PayloadAction<{
-        book: string
-        chapter: number
-        verse: string
-        shiftKey?: boolean
-      }>,
-    ) {
-      const { book, chapter, verse, shiftKey } = action.payload
-      const key = `${book}-${chapter}`
-      if (!state.selectedVerses[key]) {
-        state.selectedVerses[key] = []
-      }
-      const verses = new Set(state.selectedVerses[key])
-      if (shiftKey && state.lastSelectedVerse[key]) {
-        //
-      } else {
-        if (verses.has(verse)) {
-          verses.delete(verse)
-        } else {
-          verses.add(verse)
-        }
-        state.selectedVerses[key] = Array.from(verses)
-        state.lastSelectedVerse[key] = verse
-      }
-    },
-
     clearSelectedVerses(
       state,
       action: PayloadAction<{ book: string; chapter: number }>,
@@ -54,9 +44,35 @@ export const choosenSlice = createSlice({
       delete state.lastSelectedVerse[key]
     },
   },
+  extraReducers: builder => {
+    builder.addCase(selectVerseThunkAction.fulfilled, (state, action) => {
+      const rootState = action.payload as RootState
+      const { lang, book, chapter, verse, shiftKey } = action.meta.arg
+      const verseList =
+        rootState.books[lang]![book as BookName]!.chapters[chapter - 1]
+          .verseList
+
+      const key = `${book}-${chapter}`
+      if (!state.selectedVerses[key]) {
+        state.selectedVerses[key] = []
+      }
+      const verses = new Set(state.selectedVerses[key])
+      if (shiftKey && state.lastSelectedVerse[key]) {
+        // @todo
+      } else {
+        if (verses.has(verse)) {
+          verses.delete(verse)
+        } else {
+          verses.add(verse)
+        }
+        state.selectedVerses[key] = Array.from(verses)
+        state.lastSelectedVerse[key] = verse
+      }
+    })
+  },
 })
 
-export const { selectVerse, clearSelectedVerses } = choosenSlice.actions
+export const { clearSelectedVerses } = choosenSlice.actions
 
 // Selectors
 export const selectSelectedVerses = (
