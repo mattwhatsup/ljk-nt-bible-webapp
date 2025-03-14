f72dd6a6a4afb3e6acca4dbc9fddb5a99b2c975a
matt zhou <matt.whats.up.with.the.name@gmail.com>
2025-03-14 17:27:44 +0800

    解决auto focus和关闭dialog冲突的问题

--- src/components/JumpToDialog.tsx
+++ src/components/JumpToDialog.tsx
@@ -1,45 +1,49 @@
-import { useAppDispatch } from '@/app/hooks'
+import { useAppDispatch, useAppSelector } from '@/app/hooks'
 import {
   closeJumpToDialog,
   openJumpToDialog,
+  selectIsJumpToDialogOpen,
 } from '@/features/status/statusSlice'
 import {
   Button,
   CloseButton,
   Dialog,
+  Field,
+  Input,
   Portal,
-  useDialog,
+  Stack,
 } from '@chakra-ui/react'
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 type Props = {}
 
 export default function JumpToDialog({}: Props) {
-  const dialog = useDialog()
   const dispatch = useAppDispatch()
+  const open = useAppSelector(selectIsJumpToDialogOpen)
+  const ref = useRef<HTMLInputElement>(null)
 
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
       if ((event.ctrlKey || event.metaKey) && event.key.toLowerCase() === 'j') {
         event.preventDefault()
-        dialog.setOpen(true)
         dispatch(openJumpToDialog())
+      }
+
+      if (event.key.toLowerCase() === 'escape') {
+        event.preventDefault()
+        dispatch(closeJumpToDialog())
       }
     }
 
     window.addEventListener('keydown', handleKeyDown)
     return () => window.removeEventListener('keydown', handleKeyDown)
-  }, [dialog, dispatch])
+  }, [dispatch])
 
   return (
-    <Dialog.RootProvider
-      value={dialog}
-      onExitComplete={() => dispatch(closeJumpToDialog())}
-    >
-      {/* <Dialog.Trigger asChild>
-        <Button variant="outline" size="sm">
-          {dialog.open ? 'Close' : 'Open'} Dialog
-        </Button>
-      </Dialog.Trigger> */}
+    // <Dialog.RootProvider
+    //   value={dialog}
+    //   onExitComplete={() => dispatch(closeJumpToDialog())}
+    // >
+    <Dialog.Root initialFocusEl={() => ref.current} open={open}>
       <Portal>
         <Dialog.Backdrop />
         <Dialog.Positioner>
@@ -47,24 +51,39 @@ export default function JumpToDialog({}: Props) {
             <Dialog.Header>
               <Dialog.Title>Dialog Title</Dialog.Title>
             </Dialog.Header>
-            <Dialog.Body>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              </p>
+            <Dialog.Body pb="4">
+              <Stack gap="4">
+                <Field.Root>
+                  <Field.Label>First Name</Field.Label>
+                  <Input placeholder="First Name" />
+                </Field.Root>
+                <Field.Root>
+                  <Field.Label>Last Name</Field.Label>
+                  <Input ref={ref} placeholder="Focus First" />
+                </Field.Root>
+              </Stack>
             </Dialog.Body>
             <Dialog.Footer justifyContent="center">
               <Dialog.ActionTrigger asChild>
-                <Button variant="outline">Cancel</Button>
+                <Button
+                  variant="outline"
+                  onClick={() => dispatch(closeJumpToDialog())}
+                >
+                  Cancel
+                </Button>
               </Dialog.ActionTrigger>
               <Button>Save</Button>
             </Dialog.Footer>
             <Dialog.CloseTrigger asChild>
-              <CloseButton size="sm" />
+              <CloseButton
+                size="sm"
+                onClick={() => dispatch(closeJumpToDialog())}
+              />
             </Dialog.CloseTrigger>
           </Dialog.Content>
         </Dialog.Positioner>
       </Portal>
-    </Dialog.RootProvider>
+    </Dialog.Root>
+    // </Dialog.RootProvider>
   )
 }
