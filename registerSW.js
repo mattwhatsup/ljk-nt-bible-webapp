abc14530f49162b97fa0af35cc14a2376c40f84e
matt zhou <matt.whats.up.with.the.name@gmail.com>
2025-05-18 18:57:12 +0800

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/ljk-nt-bible-webapp/sw.js', { scope: '/ljk-nt-bible-webapp/' })})}
---
+++ sw.js
@@ -0,0 +1,109 @@
+/**
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// If the loader is already loaded, just stop.
+if (!self.define) {
+  let registry = {};
+
+  // Used for `eval` and `importScripts` where we can't get script URL by other means.
+  // In both cases, it's safe to use a global var because those functions are synchronous.
+  let nextDefineUri;
+
+  const singleRequire = (uri, parentUri) => {
+    uri = new URL(uri + ".js", parentUri).href;
+    return registry[uri] || (
+      
+        new Promise(resolve => {
+          if ("document" in self) {
+            const script = document.createElement("script");
+            script.src = uri;
+            script.onload = resolve;
+            document.head.appendChild(script);
+          } else {
+            nextDefineUri = uri;
+            importScripts(uri);
+            resolve();
+          }
+        })
+      
+      .then(() => {
+        let promise = registry[uri];
+        if (!promise) {
+          throw new Error(`Module ${uri} didn’t register its module`);
+        }
+        return promise;
+      })
+    );
+  };
+
+  self.define = (depsNames, factory) => {
+    const uri = nextDefineUri || ("document" in self ? document.currentScript.src : "") || location.href;
+    if (registry[uri]) {
+      // Module is already loading or loaded.
+      return;
+    }
+    let exports = {};
+    const require = depUri => singleRequire(depUri, uri);
+    const specialDeps = {
+      module: { uri },
+      exports,
+      require
+    };
+    registry[uri] = Promise.all(depsNames.map(
+      depName => specialDeps[depName] || require(depName)
+    )).then(deps => {
+      factory(...deps);
+      return exports;
+    });
+  };
+}
+define(['./workbox-87fefaa0'], (function (workbox) { 'use strict';
+
+  self.skipWaiting();
+  workbox.clientsClaim();
+
+  /**
+   * The precacheAndRoute() method efficiently caches and responds to
+   * requests for URLs in the manifest.
+   * See https://goo.gl/S9QRab
+   */
+  workbox.precacheAndRoute([{
+    "url": "assets/index-C8098vaa.js",
+    "revision": null
+  }, {
+    "url": "assets/index-oa8OvAR6.css",
+    "revision": null
+  }, {
+    "url": "index.html",
+    "revision": "e3ef6ec44d77b9e71d69cbac50a9d84f"
+  }, {
+    "url": "offline.html",
+    "revision": "fbe2116bafc710d9c7a2726410aae849"
+  }, {
+    "url": "registerSW.js",
+    "revision": "01a4cd97d457afb7c2b7c5c2828f05ba"
+  }, {
+    "url": "apple-touch-icon.png",
+    "revision": "43ff7b31a0acb47db42e678df0b86dd7"
+  }, {
+    "url": "favicon.ico",
+    "revision": "69ced9a8cd3ccd595b2e5fad35312a28"
+  }, {
+    "url": "manifest.webmanifest",
+    "revision": "ac3489a1437c173141854fe6bb1381f4"
+  }], {});
+  workbox.cleanupOutdatedCaches();
+  workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html")));
+
+}));
+//# sourceMappingURL=sw.js.map
