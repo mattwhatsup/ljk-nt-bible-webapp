8959e044e84ce8b9496f71bfcfa8bbf6fe610b04
matt zhou <matt.whats.up.with.the.name@gmail.com>
2025-05-18 18:56:14 +0800

    pwa config and icons

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,39 @@ import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import svgr from 'vite-plugin-svgr'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // console.log(process.env.NODE_ENV)
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), svgr()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    svgr(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: '新约圣经梁家铿译本',
+        short_name: '梁译本',
+        description: '新约圣经梁家铿译本',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   base: process.env.NODE_ENV === 'gh' ? '/ljk-nt-bible-webapp/' : '/',
   server: {
     open: true,
