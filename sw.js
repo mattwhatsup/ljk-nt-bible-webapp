4aaa47735e94d7939986dd9b959ae491b79f64a1
matt zhou <matt.whats.up.with.the.name@gmail.com>
2025-05-27 21:42:07 +0800

    Updates

--- sw.js
+++ sw.js
@@ -81,10 +81,10 @@ define(['./workbox-1a24de20'], (function (workbox) { 'use strict';
     "url": "apple-touch-icon.png",
     "revision": "43ff7b31a0acb47db42e678df0b86dd7"
   }, {
-    "url": "assets/index-BkELcrnv.js",
+    "url": "assets/index-BriEH4oY.css",
     "revision": null
   }, {
-    "url": "assets/index-C4zg-Xzb.css",
+    "url": "assets/index-DT_Gg9-_.js",
     "revision": null
   }, {
     "url": "favicon-96x96.png",
@@ -97,7 +97,7 @@ define(['./workbox-1a24de20'], (function (workbox) { 'use strict';
     "revision": "5b18e5c1f8ba1aac7345e361cc721b77"
   }, {
     "url": "index.html",
-    "revision": "412081ac2e515bc5cef3c9267ae66f95"
+    "revision": "9c0b4f02e6c744e8778a3c659a39cdc6"
   }, {
     "url": "mask-icon.svg",
     "revision": "039ceb16c1ff2548b6c0f40df839e007"
