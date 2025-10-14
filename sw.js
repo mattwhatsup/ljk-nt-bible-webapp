53c90bf0c228584212ae07c8f8628b2447d28b44
matt zhou <matt.whats.up.with.the.name@gmail.com>
2025-10-15 07:37:08 +0800

    Updates

--- sw.js
+++ sw.js
@@ -81,7 +81,7 @@ define(['./workbox-1a24de20'], (function (workbox) { 'use strict';
     "url": "apple-touch-icon-2.png",
     "revision": "5d4726e217669b9d446e2de151b1b21d"
   }, {
-    "url": "assets/index-CF6PIYlF.js",
+    "url": "assets/index-Bcdvy1ca.js",
     "revision": null
   }, {
     "url": "assets/index-kmGED2z3.css",
@@ -97,7 +97,7 @@ define(['./workbox-1a24de20'], (function (workbox) { 'use strict';
     "revision": "74333ed8c75d1f30634875c88abff2ef"
   }, {
     "url": "index.html",
-    "revision": "9d34d6d3a328c03383a68d297a47c6b7"
+    "revision": "ef43c7cf2bc6281f0452856eceb93403"
   }, {
     "url": "mask-icon.svg",
     "revision": "039ceb16c1ff2548b6c0f40df839e007"
